f41217bcad26a0b8c45a483e52bda31ca3475078
Dinis Dava <dinisdava29@gmail.com>
2025-05-04 14:56:45 +0200

    Criar frontend/store/actions/authActions.js

---
+++ frontend/store/actions/authActions.js
@@ -0,0 +1,53 @@
+const User = require('../models/User');
+const generateToken = require('../utils/generateToken');
+
+// @desc    Autenticar usuário
+// @route   POST /api/auth/login
+// @access  Public
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id)
+    });
+  } else {
+    res.status(401);
+    throw new Error('Email ou senha inválidos');
+  }
+};
+
+// @desc    Registrar novo usuário
+// @route   POST /api/auth/register
+// @access  Public
+exports.register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('Usuário já existe');
+  }
+
+  const user = await User.create({ name, email, password });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id)
+    });
+  } else {
+    res.status(400);
+    throw new Error('Dados de usuário inválidos');
+  }
+};
