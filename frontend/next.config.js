d4a6e250be9abda6c7c823ed728898ce02e057a2
Dinis Dava <dinisdava29@gmail.com>
2025-05-04 15:15:14 +0200

    Criar frontend/next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:5000/api/:path*'
+      }
+    ];
+  },
+  images: {
+    domains: ['res.cloudinary.com']
+  }
+};
