8f409d20330dfae77b209dee918a150e71ed4514
Dinis Dava <dinisdava29@gmail.com>
2025-05-04 14:53:07 +0200

    Criar backend/models/User.js

---
+++ backend/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false },
+  address: {
+    street: String,
+    city: String,
+    postalCode: String,
+    country: String
+  },
+  phone: String,
+  resetPasswordToken: String,
+  resetPasswordExpire: Date
+}, { timestamps: true });
+
+// Hash password antes de salvar
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Método para comparar senhas
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
